e9c3e3a91e7d69f4267e2e264455c90e3546f7de
Nico Krause <inspiraluna@users.noreply.github.com>
2023-03-03 14:45:09 +0530

    trying authorization code again

--- src/index.js
+++ src/index.js
@@ -31,8 +31,8 @@ app.get('/', (req, res) => {
 app.get('/getToken', async (req, res) => {
     const authorizationCode = process.env.AUTHORIZATION_CODE;  
     const client = new AuthorizationCode(credentials);
-    // const res = await getAccessToken(credentials)
-    const response = await getAccessToken(client,authorizationCode,"http://www.decentrasol.netwwork")
+    const res = await getAccessToken(credentials)
+    // const response = await getAccessToken(client,authorizationCode,"http://www.decentrasol.netwwork")
 
     //TODO FIX error: 'access_denied', 'This application is not allowed to create application tokens'
     //TODO see https://stackoverflow.com/questions/50626514/linkedin-this-application-is-not-allowed-to-create-application-tokens
